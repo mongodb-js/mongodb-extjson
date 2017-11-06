271aadd4c1929ce7f50350d9a5e6c4ab8943e19d
Dan Aprahamian <dan.aprahamian@gmail.com>
2017-11-06 11:23:28 -0500

    test(web): removed web test cases, and updated for 2.0 ext-json spec
    Removed the web test case. Also updated test to conform to 2.0 extended
    json specification

--- test/extend_mongodb_tests.js
+++ test/extend_mongodb_tests.js
@@ -18,8 +18,7 @@ function requireUncached(moduleName) {
   return require(moduleName);
 }
 
-var $Buffer = Buffer,
-  extJSON = null,
+var extJSON = null,
   mongodb = null;
 
 // BSON types
@@ -92,65 +91,6 @@ var environments = [
         doubleNumberIntFit: 19007199250000000.12
       };
     }
-  },
-  {
-    name: 'web',
-    setup: function() {
-      $Buffer = global.Buffer;
-      global.Buffer = undefined;
-      extJSON = requireUncached('..');
-    },
-    teardown: function() {
-      global.Buffer = $Buffer;
-    },
-    beforeEach: function() {
-      //mongodb = requireUncached('../../js-bson');
-      mongodb = requireUncached('bson');
-      Binary = mongodb.Binary;
-      Code = mongodb.Code;
-      DBRef = mongodb.DBRef;
-      Decimal128 = mongodb.Decimal128;
-      Double = mongodb.Double;
-      Int32 = mongodb.Int32;
-      Long = mongodb.Long;
-      MaxKey = mongodb.MaxKey;
-      MinKey = mongodb.MinKey;
-      ObjectID = mongodb.ObjectID;
-      BSONRegExp = mongodb.BSONRegExp;
-      Symbol = mongodb.Symbol;
-      Timestamp = mongodb.Timestamp;
-
-      // Create a new Buffer
-      var buffer = new Uint8Array(64);
-      for (var i = 0; i < buffer.length; i++) buffer[i] = i;
-
-      // Fix the date so test have no variable component
-      var date = new Date();
-      date.setTime(1488372056737);
-
-      test.doc = {
-        _id: new Int32(100),
-        gh: new Int32(1),
-        binary: new Binary(buffer),
-        date: date,
-        code: new Code('function() {}', { a: new Int32(1) }),
-        dbRef: new DBRef('tests', new Int32(1), 'test'),
-        decimal: Decimal128.fromString('100'),
-        double: new Double(10.1),
-        int32: new Int32(10),
-        long: Long.fromNumber(200),
-        maxKey: new MaxKey(),
-        minKey: new MinKey(),
-        objectId: ObjectID.createFromHexString('111111111111111111111111'),
-        regexp: new BSONRegExp('hello world', 'i'),
-        symbol: new Symbol('symbol'),
-        timestamp: Timestamp.fromNumber(1000),
-        int32Number: 300,
-        doubleNumber: 200.2,
-        longNumberIntFit: 0x19000000000000,
-        doubleNumberIntFit: 19007199250000000.12
-      };
-    }
   }
 ];
 
@@ -170,8 +110,9 @@ describe('Extended JSON', function() {
       it('should correctly extend an existing mongodb module', function() {
         // Serialize the document
         var json =
-          '{"_id":{"$numberInt":"100"},"gh":{"$numberInt":"1"},"binary":{"$binary":"AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0+Pw==","$type":"00"},"date":"2017-03-01T12:40:56.737Z","code":{"$code":"function() {}","$scope":{"a":{"$numberInt":"1"}}},"dbRef":{"$ref":"tests","$id":{"$numberInt":"1"},"$db":"test"},"decimal":{"$numberDecimal":"100"},"double":{"$numberDouble":"10.1"},"int32":{"$numberInt":"10"},"long":{"$numberLong":"200"},"maxKey":{"$maxKey":1},"minKey":{"$minKey":1},"objectId":{"$oid":"111111111111111111111111"},"regexp":{"$regex":"hello world","$options":"i"},"symbol":{"$symbol":"symbol"},"timestamp":{"$timestamp":"1000"},"int32Number":300,"doubleNumber":200.2,"longNumberIntFit":7036874417766400,"doubleNumberIntFit":19007199250000000}';
-        assert.equal(json, JSON.stringify(test.doc, null, 0));
+          '{"_id":{"$numberInt":"100"},"gh":{"$numberInt":"1"},"binary":{"$binary":{"base64":"AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc4OTo7PD0+Pw==","subType":"00"}},"date":{"$date":{"$numberLong":"1488372056737"}},"code":{"$code":"function() {}","$scope":{"a":{"$numberInt":"1"}}},"dbRef":{"$ref":"tests","$id":{"$numberInt":"1"},"$db":"test"},"decimal":{"$numberDecimal":"100"},"double":{"$numberDouble":"10.1"},"int32":{"$numberInt":"10"},"long":{"$numberLong":"200"},"maxKey":{"$maxKey":1},"minKey":{"$minKey":1},"objectId":{"$oid":"111111111111111111111111"},"regexp":{"$regularExpression":{"pattern":"hello world","options":"i"}},"symbol":{"$symbol":"symbol"},"timestamp":{"$timestamp":{"t":0,"i":1000}},"int32Number":{"$numberInt":"300"},"doubleNumber":{"$numberDouble":"200.2"},"longNumberIntFit":{"$numberLong":"7036874417766400"},"doubleNumberIntFit":{"$numberLong":"19007199250000000"}}';
+
+        assert.equal(json, extJSON.stringify(test.doc, null, 0));
       });
 
       it('should correctly deserialize using strict, and non-strict mode', function() {
