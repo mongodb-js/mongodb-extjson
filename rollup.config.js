f473dac2be2980fe156f800c1c392f6f82311408
Eric Daniels <eric@erdaniels.com>
2018-07-06 15:43:22 -0400

    fix(rollup): fixup exports and bundle in Buffer

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,66 @@
+import pkg from './package.json';
 import commonjs from 'rollup-plugin-commonjs';
+import nodeBuiltins from 'rollup-plugin-node-builtins';
+import nodeGlobals from 'rollup-plugin-node-globals';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
-export default {
-  input: 'index.js',
-  output: {
-    file: 'dist/mongodb-extjson.js',
-    format: 'umd',
-    name: 'mongodb-extjson',
-  },
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    babel({
-      presets: [
-        [
-          'env',
-          {
-            modules: false
-          }
-        ]
+const input = 'index.js';
+const plugins = [
+  nodeResolve(),
+  commonjs(),
+  nodeGlobals(),
+  nodeBuiltins(),
+  babel({
+    plugins: [ 'external-helpers' ],
+    presets: [
+      [
+        'env',
+        {
+          modules: false
+        }
       ]
-    })
-  ],
-  external: ['bson'],
-}
+    ]
+  })
+];
+const external = Object.keys(pkg.dependencies || {});
+const defaultName = 'mongodb-extjson';
+
+export default [
+  {
+    input,
+    output: {
+      file: 'dist/mongodb-extjson.esm.js',
+      format: 'es',
+      name: defaultName,
+      exports: 'named'
+    },
+    plugins,
+    external
+  },
+  {
+    input,
+    output: {
+      file: 'dist/mongodb-extjson.umd.js',
+      format: 'umd',
+      name: defaultName,
+      exports: 'named',
+      globals: {
+        bson: 'BSON'
+      }
+    },
+    plugins,
+    external
+  },
+  {
+    input,
+    output: {
+      file: 'dist/mongodb-extjson.browser.js',
+      format: 'iife',
+      name: defaultName,
+      exports: 'named',
+      extend: true
+    },
+    plugins
+  }
+];
