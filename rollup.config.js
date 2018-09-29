5897932161cb8abf1e6e9196a05befc0ff8a8d52
Matt Broadstone <mbroadst@gmail.com>
2018-09-29 08:30:39 -0400

    feat(bundle): bundling the module similar to js-bson
    BREAKING CHANGE: The default exported name for the module is now
    EJSON, mirroring the js-bson module's exported BSON name.

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,97 @@
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+'use strict';
 
-export default {
-  input: 'index.js',
-  output: {
-    file: 'dist/mongodb-extjson.js',
-    format: 'umd',
-    name: 'mongodb-extjson',
-  },
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    babel({
-      presets: [
-        [
-          'env',
-          {
-            modules: false
-          }
-        ]
+const pkg = require('./package.json');
+const commonjs = require('rollup-plugin-commonjs');
+const nodeBuiltins = require('rollup-plugin-node-builtins');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const babel = require('rollup-plugin-babel');
+
+const input = 'lib/ext_json.js';
+const plugins = [
+  nodeResolve(),
+  commonjs(),
+  nodeBuiltins(),
+  babel({
+    plugins: ['external-helpers'],
+    presets: [
+      [
+        'env',
+        {
+          modules: false
+        }
       ]
-    })
-  ],
-  external: ['bson'],
-}
+    ]
+  })
+];
+
+const browserPlugins = [
+  nodeResolve({
+    browser: true,
+    preferBuiltins: false
+  }),
+  commonjs(),
+  nodeBuiltins(),
+  babel({
+    plugins: ['external-helpers'],
+    presets: [
+      [
+        'env',
+        {
+          modules: false
+        }
+      ]
+    ]
+  })
+];
+
+const external = Object.keys(pkg.dependencies || {});
+const defaultName = 'EJSON';
+
+module.exports = [
+  {
+    input,
+    output: {
+      file: 'dist/mongodb-extjson.esm.js',
+      format: 'es',
+      name: defaultName,
+      exports: 'named'
+    },
+    plugins,
+    external
+  },
+  {
+    input,
+    output: {
+      file: 'dist/mongodb-extjson.browser.umd.js',
+      format: 'umd',
+      name: defaultName,
+      exports: 'named',
+      globals: {
+        buffer: 'Buffer'
+      }
+    },
+    plugins: browserPlugins,
+    external
+  },
+  {
+    input,
+    output: {
+      file: 'dist/mongodb-extjson.browser.esm.js',
+      format: 'es',
+      name: defaultName,
+      exports: 'named'
+    },
+    plugins: browserPlugins,
+    external
+  },
+  {
+    input,
+    output: {
+      file: 'dist/mongodb-extjson.bundle.js',
+      format: 'iife',
+      name: defaultName,
+      exports: 'named'
+    },
+    plugins: browserPlugins
+  }
+];
